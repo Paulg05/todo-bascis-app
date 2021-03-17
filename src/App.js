9b91f8244879c3e4c4268f78e7459a460b61e2e5
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-03-17 15:31:01 -0700

    todo rebuild

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import "./App.css";
-import TodoList from "./components/TodoList";
-// import Todo from "./components/Todo";
 
 function App() {
+  const [todos, setTodos] = useState(["task 1", "task 2", "task 3"]);
+  const [input, setInput] = useState("");
+  // console.log(input);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    console.log("im working");
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
   return (
-    <div className="app">
-      <div className="app__container">
-        <TodoList />
-      </div>
-    </div>
+    <>
+      <h1>Todo App</h1>
+      <form>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxHAwv9tgydkTiCSdkzbAUh36JifiPUb4",
+  authDomain: "todo-app-5db03.firebaseapp.com",
+  projectId: "todo-app-5db03",
+  storageBucket: "todo-app-5db03.appspot.com",
+  messagingSenderId: "208562038389",
+  appId: "1:208562038389:web:2367f977373b542ff5ba3c"
+};
